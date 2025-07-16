719e3b79a46309d114fc8792840862144cd8be17
Hong Son <hongson530.dev@gmail.com>
2025-07-16 21:30:33 +0700

    config - SecurityConfig: configured SecurityConfig to work with filter - UserProvisioning

---
+++ backend/src/main/java/com/shongon/backend/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.shongon.backend.config;
+
+import com.shongon.backend.filter.UserProvisioningFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.web.authentication.BearerTokenAuthenticationFilter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, UserProvisioningFilter userProvisioningFilter) throws Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize ->
+                        authorize.anyRequest().authenticated()
+                )
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(oauth2 ->
+                        oauth2.jwt(
+                                Customizer.withDefaults()
+                        ))
+                .addFilterAfter(userProvisioningFilter, BearerTokenAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+}
