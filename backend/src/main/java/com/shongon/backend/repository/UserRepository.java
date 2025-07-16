108e75eb130f133bac263ffd3cec614e76a50a81
Hong Son <hongson530.dev@gmail.com>
2025-07-16 21:17:40 +0700

    repo: created User repo

---
+++ backend/src/main/java/com/shongon/backend/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.shongon.backend.repository;
+
+import com.shongon.backend.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
