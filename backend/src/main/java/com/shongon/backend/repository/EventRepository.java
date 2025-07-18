0013938b888830442e53b99100cfa7c57400c989
Hong Son <hongson530.dev@gmail.com>
2025-07-18 19:33:57 +0700

    perf: rewrite query to avoid N+1 problem

--- backend/src/main/java/com/shongon/backend/repository/EventRepository.java
+++ backend/src/main/java/com/shongon/backend/repository/EventRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -13,7 +14,8 @@ import java.util.UUID;
 @Repository
 public interface EventRepository extends JpaRepository<Event, UUID> {
 
-    @EntityGraph(attributePaths = "ticketTypes")
+    @Query(value = "SELECT DISTINCT e FROM Event e LEFT JOIN FETCH e.ticketTypes t WHERE e.organizer.id = :organizerId",
+            countQuery = "SELECT COUNT(DISTINCT e) FROM Event e WHERE e.organizer.id = :organizerId")
     Page<Event> findByOrganizerId(UUID organizerId, Pageable pageable);
 
     @EntityGraph(attributePaths = "ticketTypes")
