2a9a25f3b5d6a890d987370e6b3bee5e456d5ef6
Hong Son <hongson530.dev@gmail.com>
2025-07-18 17:21:18 +0700

    fix: LazyInitialization bug in TicketType

--- backend/src/main/java/com/shongon/backend/repository/EventRepository.java
+++ backend/src/main/java/com/shongon/backend/repository/EventRepository.java
@@ -3,6 +3,7 @@ package com.shongon.backend.repository;
 import com.shongon.backend.domain.entity.Event;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +11,7 @@ import java.util.UUID;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, UUID> {
+
+    @EntityGraph(attributePaths = "ticketTypes")
     Page<Event> findByOrganizerId(UUID organizerId, Pageable pageable);
 }
